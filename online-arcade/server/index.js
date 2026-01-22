8670e5a82705e86c79249efce7eff599f50d78a9
Kaleb <kaleb.mccleary@hotmail.com>
2026-01-22 04:38:35 -0600

    Balls

---
+++ online-arcade/server/index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Database Connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Arcade Backend is Running!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
